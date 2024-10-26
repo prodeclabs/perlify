0dc392c7d758912b291e9bf97e15196cf1a238cd
Sourabh <rathoursourabh5@gmail.com>
2024-10-26 12:25:17 +0530

    chore: adding support for db adaptor using jwt session strategy

--- auth.ts
+++ auth.ts
@@ -1,7 +1,11 @@
 import NextAuth from 'next-auth'
-import GitHub from 'next-auth/providers/github'
+import { DrizzleAdapter } from '@auth/drizzle-adapter'
+import { db } from '@/db/schema'
+import authConfig from '@/auth.config'
 
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-	providers: [GitHub],
+	adapter: DrizzleAdapter(db),
+	session: { strategy: 'jwt' },
+	...authConfig,
 })
--- db/index.ts
+++ db/index.ts
@@ -6,4 +6,4 @@ import { config } from 'dotenv'
 config({ path: '.env.local' })
 
 const sql = neon(process.env.DATABASE_URL!)
-const db = drizzle({ client: sql })
+export const db = drizzle({ client: sql })
