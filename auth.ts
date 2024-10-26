9840ab3cbf1c08b90f307cd97c1455c8c18e2322
Sourabh Rathour <84420690+sourabhrathourr@users.noreply.github.com>
2024-10-26 13:00:29 +0530

    chore: set up authentication and database (#2)
    * feat: adding github provider authentication using auth.js
    * chore: setting up drizzle orm with neon database
    * chore: adding support for db adaptor using jwt session strategy
    * chore: removing explicit mention of node js runtime in authentication route.ts

---
+++ auth.ts
@@ -0,0 +1,11 @@
+import NextAuth from 'next-auth'
+import { DrizzleAdapter } from '@auth/drizzle-adapter'
+import { db } from '@/db/schema'
+import authConfig from '@/auth.config'
+
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+	adapter: DrizzleAdapter(db),
+	session: { strategy: 'jwt' },
+	...authConfig,
+})
